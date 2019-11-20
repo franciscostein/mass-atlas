c90d3cb7213d98c9b5f95c6f72e64a9c5ad818b6
leandro94costa <leandro.franc2008@hotmail.com>
2019-11-19 22:55:04 -0300

    modificado db

--- docs/db/criacao_db_test.sql
+++ docs/db/criacao_db_test.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema crud-estabelecimentos-test
 -- -----------------------------------------------------
 
@@ -18,60 +21,41 @@ USE `crud-estabelecimentos-test` ;
 -- Table `crud-estabelecimentos-test`.`categorias`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `crud-estabelecimentos-test`.`categorias` (
-  `id` INT NOT NULL,
+  `id` INT(11) NOT NULL,
   `categoria` VARCHAR(12) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `nome_UNIQUE` (`categoria` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `crud-estabelecimentos-test`.`enderecos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `crud-estabelecimentos-test`.`enderecos` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `cep` INT NULL,
-  `logradouro` VARCHAR(45) NULL,
-  `numero` INT NULL,
-  `bairro` VARCHAR(45) NULL,
-  `cidade` VARCHAR(45) NULL,
-  `estado` VARCHAR(2) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `crud-estabelecimentos-test`.`estabelecimentos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `crud-estabelecimentos-test`.`estabelecimentos` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `razao_social` VARCHAR(80) NOT NULL,
-  `nome_fantasia` VARCHAR(60) NULL,
-  `cnpj` INT NOT NULL,
-  `email` VARCHAR(45) NULL,
-  `telefone` VARCHAR(15) NULL,
-  `endereco` INT NULL,
-  `data_cadastro` DATETIME NULL,
-  `categoria` INT NULL,
-  `status` TINYINT NULL,
-  `agencia` INT NULL,
-  `conta` INT NULL,
+  `nome_fantasia` VARCHAR(60) NULL DEFAULT NULL,
+  `cnpj` VARCHAR(14) NOT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `endereco` VARCHAR(60) NULL DEFAULT NULL,
+  `cidade` VARCHAR(32) NULL,
+  `estado` VARCHAR(2) NULL,
+  `telefone` VARCHAR(11) NULL DEFAULT NULL,
+  `data_cadastro` DATETIME NULL DEFAULT NULL,
+  `categoria` INT(11) NULL DEFAULT NULL,
+  `status` TINYINT(4) NULL DEFAULT NULL,
+  `agencia` VARCHAR(4) NULL DEFAULT NULL,
+  `conta` VARCHAR(6) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `razao_social_UNIQUE` (`razao_social` ASC) VISIBLE,
   UNIQUE INDEX `cnpj_UNIQUE` (`cnpj` ASC) VISIBLE,
   INDEX `categoria_pk_idx` (`categoria` ASC) VISIBLE,
-  INDEX `endereco_pk_idx` (`endereco` ASC) VISIBLE,
   CONSTRAINT `categoria_pk`
     FOREIGN KEY (`categoria`)
-    REFERENCES `crud-estabelecimentos-test`.`categorias` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `endereco_pk`
-    FOREIGN KEY (`endereco`)
-    REFERENCES `crud-estabelecimentos-test`.`enderecos` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `crud-estabelecimentos-test`.`categorias` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
