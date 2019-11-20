763f3773c48abe7800b79f4901e2fc1d78b3644e
Leandro Costa <leandro.franc2008@hotmail.com>
2019-11-19 22:10:27 -0300

    add criacao_db_test

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,5 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.json()); // accept and convert json to object
+app.use(express.urlencoded({ extended: true }));
 
 module.exports = app;
