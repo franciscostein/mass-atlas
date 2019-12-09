61b4840a2cdce06d02576ebee19fc471d952a6ad
Leandro Costa <leandro.franc2008@hotmail.com>
2019-12-08 22:08:19 -0300

    master final

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,5 @@
-const express = require('express');
 const app = require('./app');
-const path = require('path');
 const port = process.env.PORT;
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../../client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
-    });
-}
 
 app.listen(port, () => {
     console.log(`\nServidor inicializado na porta: ${port}\n`);
