b8eba983a987ef57584449541d20528cfebe1384
Leandro Costa <leandro.franc2008@hotmail.com>
2019-12-08 22:28:43 -0300

    heroku client init

--- server/tests/categorias.test.js
+++
@@ -1,8 +0,0 @@
-const connection = require('../src/utils/db/connection');
-
-test('Buscar categorias', async () => {
-    connection.query('SELECT * FROM categorias', (error, results) => {
-        expect(error).toBeNull();
-        expect(results.length).toBe(5);
-    });
-});
--- server/tests/connection.test.js
+++
@@ -1,7 +0,0 @@
-const connection = require('../src/utils/db/connection');
-
-test('Conexão com o banco de dados', async () => {
-    connection.ping(error => {
-        expect(error).toBeNull();
-    });
-});
